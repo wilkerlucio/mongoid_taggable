160711e34f60153a52557e50f3a3108e88d70b8d
Wilker Lucio <wilkerlucio@gmail.com>
2011-01-25 00:46:24 -0300

    setup database cleaner on spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 $: << File.expand_path("../../lib", __FILE__)
 
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
 require 'mongoid'
 require 'mongoid_taggable'
 
